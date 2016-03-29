10e2aba4e4af199a4322423cc50c2e8bbf0fd446
Adam Gask <ajgask@gmail.com>
2016-03-29 14:21:36 +0100

    Updated to use the same Zip

--- scripts/download_distribution.sh
+++ scripts/download_distribution.sh
@@ -15,14 +15,15 @@ set -ev
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-j2objc_version=1.0.1.1
+j2objc_version=1.0.1
+tag=1.0.1.1
 
 if [[ -d dist ]]; then
   exit
 fi
 
 echo "fetching j2objc"
-curl -OL https://github.com/AJ9/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
+curl -OL https://github.com/AJ9/j2objc/releases/download/${tag}/j2objc-1.0.1.zip
 mkdir dist
-unzip -q j2objc-${j2objc_version}.zip
+unzip -q j2objc-1.0.1.zip
 rm -rf __MACOSX
